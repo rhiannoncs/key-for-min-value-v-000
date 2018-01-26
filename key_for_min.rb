9c2d0d381fbc4057d855eb9c25458ab227f2e50d
Rhiannon Shoults <rhiannon.csw@gmail.com>
2018-01-26 03:00:27 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,7 +4,7 @@
 def key_for_min_value(name_hash)
   min_value = nil
   min_key = nil
-  unless name_hash.empty?
+  unless name_h
     name_hash.each do |name, value|
       if min_value == nil || value < min_value
         min_value = value
